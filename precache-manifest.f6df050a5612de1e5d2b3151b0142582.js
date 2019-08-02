b3650b10c99e335445060012f23c66a4832eb7bb
Roman <rmnlaiacona@gmail.com>
2019-08-01 20:55:14 -0400

    Updates

---
+++ precache-manifest.f6df050a5612de1e5d2b3151b0142582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcf1125845e9a20371e3fe8ca3a3269",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cee69ad2010d5b9742fd",
+    "url": "/monsters-rolodex/static/css/main.35cc5ce8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cee69ad2010d5b9742fd",
+    "url": "/monsters-rolodex/static/js/main.e8124b92.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);

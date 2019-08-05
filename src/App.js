d3e2ea5288f31599b9ed6678751e70c55eb86234
Roman <rmnlaiacona@gmail.com>
2019-08-05 13:26:10 -0400

    fixing https secure connection in links

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
 
   //Setting the 'monsters' to an array of json data 
   componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response =>
       response.json()).
       then(users=>this.setState({monsters:users}));
